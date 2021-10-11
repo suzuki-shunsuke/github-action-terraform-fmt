f6517a0a54e3dee40b7a5d599e7e21cca51edfcc
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-10-11 11:31:26 +0900

    feat: implement basic functions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # github-action-terraform-fmt
+
 GitHub Actions to run `terraform fmt`
+
+## Requirements
+
+* terraform
+* [github-comment](https://github.com/suzuki-shunsuke/github-comment)
+* [ghcp](https://github.com/int128/ghcp)
+
+## License
+
+[MIT](LICENSE)
---
+++ main.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+if terraform fmt -check -recursive > /dev/null 2>&1; then
+  exit 0
+fi
+
+tempfile=$(mktemp)
+github-comment exec -- terraform fmt -recursive | sed "s|^|$WORKING_DIR|" > "$tempfile"
+# shellcheck disable=SC2046
+github-comment exec -- \
+  ghcp commit -r "$GITHUB_REPOSITORY" -b "$GITHUB_HEAD_REF" \
+  -m "style: terraform fmt -recursive" \
+  -C "$ROOT_DIR" $(cat "$tempfile") \
+  --token "$GITHUB_APP_TOKEN"
+rm "$tempfile"
+exit 1
